77da8cac4cc22374eff44cb6b4cc1b473b3ca64a
Alvin Man <alvin.man28@gmail.com>
2016-02-18 00:09:55 -0800

    - Homepage displays database queries on separate rows. - Added several helper functions to Main controller. - Added several helper functions to StockModel model.

--- application/models/StockModel.php
+++ application/models/StockModel.php
@@ -3,62 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class StockModel extends CI_Model {
 
-
-	
-	//
 	public function getPlayerInfo(){
 		$this->db->select('*');
 		$this->db->from('players');
 		$query=$this->db->get();
 		return $query->result_array();
-		/*
-		echo "<h2>Player Information </h2>";
-		echo "<table>";
-		foreach ($query->result_array() as $row)
-		{
-			echo "<tr>";
-			echo "<td>";
-  	 		echo $row['Player'];
-  	 		echo "</td>";
-  	 		echo "<td>";
-   			echo $row['Cash'];
-   			echo "</td>";
-   			echo "</tr>";
-
-		}
-		echo "</table>";
-		*/
-
-
 	}
 	public function getStockInfo(){
 		$this->db->select('*');
 		$this->db->from('stocks');
 		$query=$this->db->get();
 		return $query->result_array();
-		/*
-		echo "<h2>Stock information</h2>";
-		echo "<table>";
-		foreach ($query->result_array() as $row)
-		{
-			echo "<tr>";
-			echo "<td>";
-  	 		echo $row['Code'];
-  	 		echo "</td>";
-  	 		echo "<td>";
-   			echo $row['Name'];
-   			echo "</td>";
-   			echo "<td>";
-   			echo $row['Category'];
-   			echo "</td>";
-   			echo "<td>";
-   			echo $row['Value'];
-   			echo "</td>";
-   			echo "</tr>";
-		}
-		echo "</table>";
-		*/
-
 	}
 
 	public function getStockInfoRange($count, $from){
@@ -68,16 +23,14 @@ class StockModel extends CI_Model {
 		$query=$this->db->get();
 		return $query->result_array();
 	}
-	
 
-	
-
-
+	public function getTotalStockNum(){
+		$this->db->select('*');
+		$this->db->from('stocks');
+		$query=$this->db->get();
+		return $query->num_rows();
+	}
 }
-
-
-/*
-
 
 		/*
 		$this->db->select('*');
--- application/views/Player.php
+++ application/views/Player.php
@@ -2,3 +2,5 @@
 <a href="/">Home Page</a>
 </br>
 <a href="/stockhistory">Stock History</a>
+
+
