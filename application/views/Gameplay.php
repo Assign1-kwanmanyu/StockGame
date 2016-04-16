2007cf91942b7a5b369e2df36ef4ffa6e2d2a245
Alvin Man <alvin.man28@gmail.com>
2016-04-16 15:47:26 -0700

    Updated gameplay page.

--- application/views/Gameplay.php
+++ application/views/Gameplay.php
@@ -9,12 +9,115 @@
                     </div>
                 </div>
 
+                <div class="row">
+
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h3 class="panel-title"><i class="fa fa-clock-o fa-fw"></i> Game Summary</h3>
+                            </div>
+                            <div class="panel-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                        <tr>
+                                            <th>Round</th>
+                                            <th>Status</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <th>{round}</th>
+                                            <th>{desc}</th>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="text-right">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div>
-                    <h2>Current Market Status</h2>
-                    <ul>
-                        <li>Round: {round}</li>
-                        <li>Status: {desc}</li>
-                    </ul>
+                    <h2>Current Stocks</h2>
+
+                    <?php $counter = 0; ?>
+                    <div class = "row div-stock-dash-wrapper padding-top-5">
+                        <?php for ($row = 1; $row < count($currentStocks); $row ++){?>
+
+                            <?php
+                            //generate a new row if the counter reaches 4
+                            if($counter==4){
+                                echo '</div>';
+                                echo '<div class = "row div-stock-dash-wrapper padding-top-5">';
+                                $counter = 0;
+                            }
+                            ?>
+
+                            <div class = "col-md-3 norm">
+                                <a href="/stockhistory/<?php echo $currentStocks[$row]['code']; ?>">
+                                    <div class= "row <?php
+                                    if ($currentStocks[$row]['category'] == 'A') { echo 'stock-up'; }
+                                    else if ($currentStocks[$row]['category'] == 'B') { echo 'stock-up-2'; }
+                                    else if ($currentStocks[$row]['category'] == 'C') { echo 'stock-down'; }?>">
+                                        <div class = "col-md-12">
+                                            <h2><?php echo $currentStocks[$row]['code'] ?></h2>
+                                            <span class = "span-stock-dash-subtitle"><?php echo $currentStocks[$row]['name'] ?></span>
+                                            <div class= "row">
+                                                <div class = "col-md-6">
+                                                    <h1><?php echo $currentStocks[$row]['value'] ?></h1>
+                                                </div>
+                                                <div class = "col-md-6 text-right">
+                                                    <span><?php echo $currentStocks[$row]['category'] ?></span>
+                                                    <span><?php echo $currentStocks[$row]['value'] ?></span>
+                                                </div>
+                                            </div>
+                                            <?php $counter++; ?>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <?php
+                        } ?>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h2>Select an Active stock to purchase -></h2>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="dropdown" style = "margin-top: 10px;    ">
+                            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Purchase Active Stocks
+                                <span class="caret"></span></button>
+                            <ul class="dropdown-menu">
+                                {actStocks}
+                                <li><a href="/Stock{code}">{code}</a></li>
+                                {/actStocks}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                </br>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h2>Select an Active stock to Sell -></h2>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="dropdown" style = "margin-top: 10px;    ">
+                            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Sell Active Stocks
+                                <span class="caret"></span></button>
+                            <ul class="dropdown-menu">
+                                {actStocks}
+                                <li><a href="/Stocksell{code}">{code}</a></li>
+                                {/actStocks}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
 
                 <div>
@@ -54,28 +157,6 @@
                         </div>
                     </div>
                 </div>
-
-
-
-            </div>
-            <div class="div-friend-list col-md-4" style = "background: #2E3136;">
-                <h2>Online - 4</h2>
-                {PlayerInfo}
-                <div class = "row">
-                    <div class = "col-md-12">
-                        <ul class="ul-friend-list list-unstyled">
-                            <a href="/player/{Player}">
-                                <li class=""><img src = "/assets/img/friend1.png">{Player}
-                            </a>
-                            <ul class = "ul-friend-info list-unstyled">
-                                <li><img src = "/assets/img/icon-cash.png">{Cash}</li>
-                                <li><img src = "/assets/img/icon-stock-muney.png">{Equity}</li>
-                            </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                {/PlayerInfo}
             </div>
         </div>
     </div>
