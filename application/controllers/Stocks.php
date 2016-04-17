49d6d11178e0730812027065f6004304c62908bb
Alvin Man <alvin.man28@gmail.com>
2016-04-17 02:26:41 -0700

    Added method to register agent and return token.

--- application/controllers/Stocks.php
+++ application/controllers/Stocks.php
@@ -26,5 +26,31 @@ class Stocks extends MY_Controller {
         $this->render();
     }
 
+    public function getAgentToken(){
+        $url = 'http://www.comp4711bsx.local/register';
+        $data = array('team' => "o01",
+            'name' => "x3",
+            'password' => "tuesday"
+        );
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+
+        if ($result === FALSE) { return "unavailable"; }
+
+        $xml = simplexml_load_string($result);
+
+        return $xml->token;
+
+    }
+
 
 }
